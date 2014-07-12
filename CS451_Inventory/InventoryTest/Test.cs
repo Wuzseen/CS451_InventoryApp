88a75ce61b60e3749f31f42c67bc1a794ac34ccc
Timothy Day <the.tday@gmail.com>
2014-07-12 11:00:56 -0400

    Basic sample code and unit tests in.

--- CS451_Inventory/InventoryTest/Test.cs
+++ CS451_Inventory/InventoryTest/Test.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using System;
+using Inventory;
 
 namespace InventoryTest
 {
@@ -7,8 +8,13 @@ namespace InventoryTest
 	public class Test
 	{
 		[Test ()]
-		public void TestCase ()
+		public void ItemNameIsValid ()
 		{
+			string testName = "WalrusSnacks";
+			InventoryItem i = new InventoryItem ();
+			Assert.AreEqual("InvItem", i.Name);
+			i = new InventoryItem(testName);
+			Assert.AreEqual(testName, i.Name);
 		}
 	}
 }
