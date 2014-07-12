519b2c210229afac04ee384a28550a799a6b0769
Timothy Day <the.tday@gmail.com>
2014-07-12 12:07:09 -0400

    Folders named appropriately.

---
+++ CS451_Inventory/InventoryController/InventoryItem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace InventorySystem {
+	public class InventoryItem {
+		private string name;
+		public string Name {
+			get { return name; }
+			set { name = value; }
+		}
+
+		private static int IDCount = 1;
+		private int id;
+		public int ID {
+			get { return id; }
+		}
+
+		private void Setup() {
+			id = IDCount;
+			IDCount++;
+		}
+
+		public InventoryItem () {
+			Setup();
+			name = "InvItem";
+		}
+
+		public InventoryItem(string name) {
+			Setup();
+			this.name = name;
+		}
+	}
+}
+
