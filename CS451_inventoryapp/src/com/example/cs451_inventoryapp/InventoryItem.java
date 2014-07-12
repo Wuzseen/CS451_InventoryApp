57f858f39a8d5e7ac71732ce9516a9aa06324cd0
Timothy Day <the.tday@gmail.com>
2014-07-12 14:52:59 -0400

    Inventory Item class implemented.

--- CS451_inventoryapp/src/com/example/cs451_inventoryapp/InventoryItem.java
+++ CS451_inventoryapp/src/com/example/cs451_inventoryapp/InventoryItem.java
@@ -1,5 +1,32 @@
 package com.example.cs451_inventoryapp;
 
 public class InventoryItem {
-
+	private static int idCount = 1;
+	
+	private int id;
+	public int getId() {
+		return id;
+	}
+	
+	private String name;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	private void Setup(String itemName) {
+		this.id = idCount;
+		idCount++;
+		this.name = itemName;
+	}
+	
+	public InventoryItem(String itemName) {
+		this.Setup(itemName);
+	}
+	
+	public InventoryItem() {
+		this("DefaultInventoryItem");
+	}
 }
