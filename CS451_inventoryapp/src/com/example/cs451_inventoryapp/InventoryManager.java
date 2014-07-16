e4ff19b2d13af497b992735461f8be0230f98487
Timothy Day <the.tday@gmail.com>
2014-07-16 17:46:31 -0400

    Some things.

--- CS451_inventoryapp/src/com/example/cs451_inventoryapp/InventoryManager.java
+++ CS451_inventoryapp/src/com/example/cs451_inventoryapp/InventoryManager.java
@@ -1,5 +1,18 @@
 package com.example.cs451_inventoryapp;
 
-public class InventoryManager {
+import java.util.ArrayList;
 
+// Handles heirarchy of items and locations
+public class InventoryManager {
+	private ArrayList<Location> locationListing;
+	private ArrayList<InventoryItem> invItems;
+	
+	public InventoryManager() {
+		locationListing = new ArrayList<Location>();
+		invItems = new ArrayList<InventoryItem>();
+	}
+	
+	public void AddLocation(Location l) {
+		locationListing.add(l);
+	}
 }
